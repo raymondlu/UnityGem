0d6b7df8867d6eaec628f5968fdcb571ce9ab0ca
raymondlu <raymond-lu@live.com>
2016-11-18 20:17:52 +0800

    实现加载场景（未完）
    增加了背景，游戏标题，进度条。

---
+++ Gem/Assets/Scripts/LoadingBar.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LoadingBar : MonoBehaviour {
+	public Image barImage;
+	public int percent = 50;
+
+	void InitReferences(){
+		Transform imageBarTransform = transform.Find ("ImageProgressBar");
+		if (imageBarTransform) {
+			barImage = imageBarTransform.gameObject.GetComponent<Image> ();
+		}
+
+		if (!barImage) {
+			Debug.LogError ("Failed to locate image bar component");	
+		}
+	}
+
+	void UpdateProgress(){
+		float currentAmount = percent / 100f;
+		barImage.fillAmount = currentAmount;
+	}
+
+	public void SetPercent(int newPercent) {
+		percent = Mathf.Clamp (newPercent, 0, 100);
+		UpdateProgress ();
+	}
+
+	void Reset() {
+		InitReferences ();
+		SetPercent (percent);
+	}
+
+	// Use this for initialization
+	void Start () {
+		InitReferences ();
+		UpdateProgress ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
