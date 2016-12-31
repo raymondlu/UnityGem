4f6c560eb80109da7b6d2fa64bebeb1654b680fb
raymondlu <raymondlu1105@qq.com>
2016-12-31 17:59:48 +0800

    将游戏场景里摄像机的纵横比设置成设计尺寸的纵横比
    如题。

---
+++ Gem/Assets/Scripts/CameraAspactRation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraAspactRation : MonoBehaviour {
+	public int canvasWidth = 640;
+	public int canvasHeight = 1136;
+	// Use this for initialization
+	void Start () {
+		Debug.Log (string.Format("Current screen size:{0}x{1}",Screen.width,Screen.height));
+		Camera.main.aspect = (float)canvasWidth / canvasHeight;
+		Debug.Log ("Main camera aspact ration:" + Camera.main.aspect);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
