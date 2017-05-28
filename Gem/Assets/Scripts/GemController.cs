ed14ec7889077414e9b53ca602bf77d62ea6bf36
raymondlu <raymondlu1105@qq.com>
2017-05-28 17:25:39 +0800

    增加宝石控制脚本
    如题

---
+++ Gem/Assets/Scripts/GemController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GemController : MonoBehaviour
+{
+    private Renderer rend;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rend = GetComponent<Renderer>();	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        
+    }
+    private void OnMouseUp()
+    {
+        
+    }
+    private void OnMouseEnter()
+    {
+        //rend.material.color = Color.red;
+    }
+    private void OnMouseOver()
+    {
+        //rend.material.color -= new Color(0.1F, 0, 0) * Time.deltaTime;
+        rend.material.color = Color.red;
+    }
+    private void OnMouseExit()
+    {
+        rend.material.color = Color.white;
+    }
+}
